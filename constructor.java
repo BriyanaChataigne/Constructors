0a339f70a2564ba924ee150a2f6244c88daa41e0
BriyanaChataigne <briyana.chataigne@techhigh.us>
2015-08-07 20:40:50 -0400

    added constructor.java

---
+++ constructor.java
@@ -0,0 +1,8 @@
+ public class apples {
+		public static void main (String args[]){
+		nutella bread = new nutella ("Edward Elric");
+		nutella toast = new nutella ("Future Trunks");
+		bread.saying();
+		toast.saying();
+		}
+	}
